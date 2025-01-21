e498767fd757ead897b043ad05d5104f741959d2
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-01-21 15:46:59 +0530

    Day-18 Operators and Conversions in Javascript

---
+++ Day-18-JS/OperatorsAndConversion/OperatorsAndConversion.js
@@ -0,0 +1,45 @@
+// Implicit Conversion - (Automatically Conversion)
+// String Conversion
+console.log(" ");
+console.log("String Conversion");
+console.log(`"5" + 5`);
+console.log("5" + 5);
+console.log(typeof ("5" + 5));
+
+// Number Conversion
+console.log(" ");
+console.log("Number Conversion");
+console.log(`"5" - 5`);
+console.log("5" - 5);
+console.log(typeof ("5" - 5));
+
+// Boolean Conversion
+console.log(" ");
+console.log("Boolean Conversion");
+console.log(true);
+console.log(typeof true);
+
+console.log(false);
+console.log(typeof false);
+
+// Comparison Conversion
+console.log(" ");
+console.log("Comparison Conversion");
+console.log(`"5" == 5`);
+console.log("5" == 5); // both represent "true" boolean value
+console.log(typeof ("5" == 5));
+
+console.log(" ");
+console.log(`null == undefined`);
+console.log(null == undefined); // both represent "false" boolean value
+console.log(typeof (null == undefined));
+
+console.log(" ");
+console.log(`true == 0`);
+console.log(true == 0); // "true" represent true boolean value, While 0 represent "false" boolean value.
+console.log(typeof (true == 0));
+
+console.log(" ");
+console.log(`empty-string == space-string`);
+console.log("" == " "); // "" represent false boolean value, While " " represent "true" boolean value.
+console.log(typeof ("" == " "));
