d1dd1ae9b98f9a4df720b595e1dece24c1fce979
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-01-21 17:10:51 +0530

    Day-18 Type Conversions in Javascript

--- Day-18-JS/Type-Conversion/type-conversion.js
+++ Day-18-JS/Type-Conversion/type-conversion.js
@@ -2,16 +2,15 @@
 // String Conversion
 console.log(" ");
 console.log("String Conversion");
-console.log(`"5" + 5`);
-console.log("5" + 5);
-console.log(typeof ("5" + 5));
+console.log(`"5" + 5: `, "5" + 5);
+// Explicit Conversion
+console.log(`Number("5") + 5: `, Number("5") + 5);
+console.log(`Number("5") + String(5): `, Number("5") + String(5));
 
 // Number Conversion
 console.log(" ");
 console.log("Number Conversion");
-console.log(`"5" - 5`);
-console.log("5" - 5);
-console.log(typeof ("5" - 5));
+console.log(`"5" - 5: `, "5" - 5);
 
 // Boolean Conversion
 console.log(" ");
@@ -25,21 +24,48 @@ console.log(typeof false);
 // Comparison Conversion
 console.log(" ");
 console.log("Comparison Conversion");
-console.log(`"5" == 5`);
-console.log("5" == 5); // both represent "true" boolean value
+console.log(`"5" == 5: `, "5" == 5); // both represent "true" boolean value
 console.log(typeof ("5" == 5));
 
 console.log(" ");
-console.log(`null == undefined`);
-console.log(null == undefined); // both represent "false" boolean value
-console.log(typeof (null == undefined));
+console.log(`null == undefined: `, null == undefined); // both represent "false" boolean value
+console.log(`Typeof null: `, typeof null);
+console.log(`Typeof undefined: `, typeof undefined);
+
+// Strict Equality (===)
+console.log(" ");
+console.log(`Strict Equality - (===)`);
+console.log(`null === undefined: `, null === undefined);
+console.log(typeof (null === undefined));
 
 console.log(" ");
-console.log(`true == 0`);
-console.log(true == 0); // "true" represent true boolean value, While 0 represent "false" boolean value.
-console.log(typeof (true == 0));
+console.log(`true == 0: `, true == 0); // "true" represent true boolean value, While 0 represent "false" boolean value.
 
 console.log(" ");
-console.log(`empty-string == space-string`);
-console.log("" == " "); // "" represent false boolean value, While " " represent "true" boolean value.
-console.log(typeof ("" == " "));
+console.log(`true == 1: `, true == 1); // "true" represent true boolean value, While 1 represent "false" boolean value.
+
+console.log(" ");
+console.log(`true === 1: `, true === 1); // "true" represent true boolean value, While 1 represent "false" boolean value.
+console.log(`true - Boolean Type`);
+console.log(`1 - Number`);
+
+console.log(" ");
+console.log(`empty-string("") == space-string(" ")`, "" == " "); // "" represent false boolean value, While " " represent "true" boolean value.
+console.log(`Typeof "": `, typeof "");
+console.log(`Typeof " ": `, typeof " ");
+// Explicit Conversion
+console.log(`Boolean Value of "": `, Boolean(""));
+console.log(`Boolean Value of " ": `, Boolean(" "));
+
+// Strict Equality (===)
+console.log(" ");
+console.log(`"5" == NaN: `, "5" == NaN); // "" represent false boolean value, While " " represent "true" boolean value.
+console.log(`Typeof NaN: `, typeof NaN);
+
+// Parsing Numbers
+console.log(" ");
+console.log(`parseInt("123abc"): `, parseInt("123abc"));
+console.log(`parseInt("abc123"): `, parseInt("abc123"));
+
+console.log(" ");
+console.log(`parseFloat("3.14abc"): `, parseFloat("3.14abc"));
