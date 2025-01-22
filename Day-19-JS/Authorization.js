e9b1d3dcc3ef166f7eaeef7154c2562add0406b0
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-01-22 11:45:56 +0530

    Day-19 Authorization [if-else-nested] in Javascript

---
+++ Day-19-JS/Authorization.js
@@ -0,0 +1,29 @@
+let age = 18;
+let role = "student";
+let hasId = true; // hasId must "boolean" typed for this.
+
+// hasId type checked if "boolean"
+if (typeof hasId === "boolean") {
+  // token must be "true" in boolean type to procced ahead.
+  let token = hasId;
+  // age verification
+  if (age >= 18) {
+    // role verification
+    if (role === "student") {
+      // token verification
+      if (token) {
+        console.log("Access Granted.");
+      } else {
+        console.log("Sorry, you do not have a valid ID.");
+      }
+    } else {
+      console.log("Access Denied!! You may not authorized.");
+    }
+  } else {
+    console.log(
+      "Sorry your age does not meet the criteria that was minimum of age(18) and above."
+    );
+  }
+} else {
+  console.log("Something wen wrong! ID type may not be matched.");
+}
