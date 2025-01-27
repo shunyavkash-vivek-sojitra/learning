281b0c777566491e63c1c87076cd032da60f6540
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-01-27 14:03:23 +0530

    Day-21-22 Try-Catch-Finally Function in Javascript

---
+++ Day-21-22-JS/Try-Catch-Finally/try-catch-finally.js
@@ -0,0 +1,23 @@
+let age = 18;
+
+const verifyAge = () => {
+  try {
+    if (typeof age === typeof 18) {
+      if (age >= 18) {
+        console.log("Welcome, you are eligible for voting, Your AGE: ", age);
+      } else {
+        console.log(
+          `Your age:${age} does not meet the minimum age criteria for voting!!`
+        );
+      }
+      return;
+    }
+    throw new Error(
+      "Error occured with age!! - [ age must be in Number type. ] "
+    );
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+verifyAge(age);
