ab13507df9a321d7e0926799301191009cb9318b
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-02-05 12:57:32 +0530

    Day-29 DOM - [TODO] in Javascript

--- Day-29-JS/service/service.js
+++ Day-29-JS/service/service.js
@@ -1,15 +1,19 @@
-// to display todo
 let todoDisplay = document.querySelector("#todoDisplay");
-
-// to create new todo
 let todoInput = document.querySelector("#todo-input");
-let todoTextarea = document.querySelector("#todo-text-area");
+let btnSubmit = document.querySelector("#btnSubmit");
+
+const getTodos = () => {
+  return JSON.parse(localStorage.getItem("Todo")) || [];
+};
+
+const setTodos = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+console.log("Get Todos: ", getTodos());
 
 let todos = [];
-
-const getTodos = JSON.parse(localStorage.getItem("Todo"));
-
-console.log("Get Todos: ", getTodos);
+todos = getTodos();
 
 // ====================================================== Create Todo
 // to create new todo
@@ -17,43 +21,65 @@ const todoCreated = () => {
   event.preventDefault();
 
   // fetching data from from
-  const todoNew = new Object({
-    todoTitle: todoInput.value,
-    todoDescription: todoTextarea.value,
-  });
+  const todoNew = todoInput.value.trim();
+  console.log("todoNew: ", todoNew);
 
   //  pushed "todoNew{}" into array "todos[]"
-  // geteTodos might works
   todos.push(todoNew);
-
   // set array "todos[]" to localStorage
-  localStorage.setItem("Todo", JSON.stringify(todos));
-
+  setTodos("Todo", todos);
   // clear value of inputs at the end.
   todoInput.value = "";
-  todoTextarea.value = "";
-
-  // call todoDisplay
-  todoDisplayed(todoNew.todoTitle, todoNew.todoDescription);
+  todoDisplayed();
 };
+
+// onSubmit
+btnSubmit.addEventListener("click", () => {
+  todoCreated();
+});
 
 // ====================================================== Display Todo
-const todoDisplayed = (title, description) => {
-  todoDisplay.innerHTML = getTodos
-    .map((todo) => {
-      return `<li>
-                                    <div class="todo-flex-wrap">
-                                        <h4 id="todo-title">${todo.todoTitle}</h4>
-                                        <p id="todo-desctiption">${todo.todoDescription}</p>
-                                    </div>
-                                    <div class="todo-flex-wrap">
-                                        <button onclick="todoDeleted()">
-                                            <i class="fa-regular fa-trash-can"></i>
-                                        </button>
-                                    </div>
-                                </li>`;
-    })
-    .join("");
+const todoDisplayed = () => {
+  todos = getTodos();
+  todoDisplay.innerHTML = "";
+  todos.length == 0
+    ? (todoDisplay.innerHTML = `<h4 class="todo-not-fount">No Todos Found</h4>`)
+    : todos.forEach((todo) => {
+        todoDisplay.innerHTML += `<li>
+                                  <div class="todo-flex-wrap">
+                                      <h4 class="todo-title text-elepse">${todo}</h4>
+                                  </div>
+                                  <div class="todo-flex-wrap" id="todo-flex-wrap">
+                                      <button class="btnDelete">
+                                         <i class="fa-solid fa-xmark"></i>
+                                      </button>
+                                  </div>
+                              </li>`;
+      });
+};
+// call todoDisplay
+todoDisplayed();
+
+// ====================================================== Delete Todo
+let deleteTodo = (e) => {
+  const fetchTodo = e.target.closest("li");
+  console.log("fetched Todo: ", fetchTodo);
+  // if (!fetchTodo) return;
+
+  const todoText = fetchTodo.querySelector(".todo-title").textContent.trim();
+  console.log("todo text: ", todoText);
+
+  const todoDeleted = todos.filter((currentTodo) => {
+    return currentTodo !== todoText;
+  });
+  console.log("updated Todos", todoDeleted);
+  setTodos("Todo", todoDeleted);
+  todoDisplayed();
 };
 
-// document.onload();
+// toDeleteTodo
+todoDisplay.addEventListener("click", (e) => {
+  if (e.target.closest(".btnDelete")) {
+    deleteTodo(e);
+  }
+});
