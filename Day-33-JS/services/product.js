473b56287abbb7c1ab7be1f2ce827fb93d8ea54b
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-02-18 17:27:27 +0530

    Day-33 DOM - Product Management [Dynamic Content] in Javascript

--- Day-33-JS/services/product.js
+++ Day-33-JS/services/product.js
@@ -1,6 +1,7 @@
 // form
 let productFormReset = document.querySelector("#productForm");
 let productForm = document.querySelectorAll("#productForm");
+let inputField = document.getElementsByTagName("input");
 let tableBody = document.querySelector("#tbody");
 
 // inputs
@@ -12,12 +13,31 @@ let rating = document.querySelector("#rating");
 // add button
 let btnAdd = document.querySelector(".btnAdd");
 
-// search & filter
+// search
 let searchBox = document.querySelector("#searchBox");
+
+// filter
 let filterCategory = document.querySelector("#filterCategory");
+let selectOption = document.querySelector("#selectOption");
 
 // table
 let dataTable = document.querySelector("#productTable");
+
+// TH Sorting
+// id
+let reOrder = document.querySelector("#reorder");
+// name
+let nameUp = document.querySelector("#name-up");
+let nameDown = document.querySelector("#name-down");
+// price
+let priceUp = document.querySelector("#price-up");
+let priceDown = document.querySelector("#price-down");
+// category
+let categoryUp = document.querySelector("#category-up");
+let categoryDown = document.querySelector("#category-down");
+// rating
+let ratingUp = document.querySelector("#rating-up");
+let ratingDown = document.querySelector("#rating-down");
 
 // setData
 const setData = (key, data) => {
@@ -30,15 +50,38 @@ const getData = () => {
 };
 
 // Products
-const products = getData();
-// console.log(products);
+let products = getData();
+
+// Default "All Categories" & Dynamic categories choice
+const filterOptions = () => {
+  const categories = [...new Set(products.map((product) => product.category))];
+  filterCategory.innerHTML = ` <option value="All Categories">All Categories</option>
+  ${categories
+    .map((category) => `<option value="${category}">${category}</option>`)
+    .join("")}`;
+};
+
+filterOptions();
 
 // =================================================== Create Product /
 const createProduct = (e) => {
   e.preventDefault();
 
+  // fields required
+  if (
+    !name.value.trim() ||
+    !price.value.trim() ||
+    !category.value.trim() ||
+    !rating.value.trim()
+  ) {
+    alert("All fields are must be filled!");
+    return;
+  }
+
+  // new data
   const newProduct = {
-    id: "id" + Math.random().toString(16).slice(2),
+    // id: "id" + Math.random().toString(16).slice(2),
+    id: products.length + 1,
     name: name.value.trim(),
     price: price.value.trim(),
     category: category.value.trim(),
@@ -49,67 +92,111 @@ const createProduct = (e) => {
   setData("Product", products);
   productFormReset.reset();
 
-  // console.log(newProduct);
+  filterOptions();
   displayProduct();
 };
 
 // =================================================== Display Product /
-const displayProduct = () => {
-  products.forEach((product) => {
-    const tableRow = tableBody.insertRow();
-    tableRow.insertCell(0).textContent = product.id;
-    tableRow.insertCell(1).textContent = product.name;
-    tableRow.insertCell(2).textContent = product.price;
-    tableRow.insertCell(3).textContent = product.category;
-    tableRow.insertCell(4).textContent = product.rating;
-  });
+const displayProduct = (reqAction) => {
+  // clear table before load data
+  tableBody.innerHTML = "";
+
+  // load data
+  if (!reqAction == "") {
+    reqAction.forEach((product) => {
+      const tableRow = tableBody.insertRow();
+      tableRow.insertCell(0).textContent = product.id;
+      tableRow.insertCell(1).textContent = product.name;
+      tableRow.insertCell(2).textContent = product.price;
+      tableRow.insertCell(3).textContent = product.category;
+      tableRow.insertCell(4).textContent = product.rating;
+    });
+  } else {
+    products.forEach((product) => {
+      const tableRow = tableBody.insertRow();
+      tableRow.insertCell(0).textContent = product.id;
+      tableRow.insertCell(1).textContent = product.name;
+      tableRow.insertCell(2).textContent = product.price;
+      tableRow.insertCell(3).textContent = product.category;
+      tableRow.insertCell(4).textContent = product.rating;
+    });
+  }
 };
 
 displayProduct();
 
 // =================================================== Sorting /
+// id - reOrder
+reOrder.addEventListener("click", () => {
+  const idSorted = products.sort((a, b) => a.id - b.id);
+  displayProduct(idSorted);
+});
+
 // name sorting
-const sortByName = () => {
+nameUp.addEventListener("click", () => {
   const nameSorted = products.sort((a, b) => a.name.localeCompare(b.name));
-  console.table("Sorted By Name:");
-  console.table(nameSorted);
-};
-// sortByName();
+  displayProduct(nameSorted);
+});
+
+nameDown.addEventListener("click", () => {
+  const nameSorted = products.sort((a, b) => b.name.localeCompare(a.name));
+  displayProduct(nameSorted);
+});
 
 // price sorting
-const sortByPrice = () => {
+priceUp.addEventListener("click", () => {
   const priceSorted = products.sort((a, b) => a.price - b.price);
-  console.table("Sorted By Price:");
-  console.table(priceSorted);
-};
-// sortByPrice();
+  displayProduct(priceSorted);
+});
+
+priceDown.addEventListener("click", () => {
+  const priceSorted = products.sort((a, b) => b.price - a.price);
+  displayProduct(priceSorted);
+});
 
 // category sorting
-const sortByCategory = () => {
+categoryUp.addEventListener("click", () => {
   const categorySorted = products.sort((a, b) =>
     a.category.localeCompare(b.category)
   );
-  console.table("Sorted By Category:");
-  console.table(categorySorted);
-};
-// sortByCategory();
+  displayProduct(categorySorted);
+});
+
+categoryDown.addEventListener("click", () => {
+  const categorySorted = products.sort((a, b) =>
+    b.category.localeCompare(a.category)
+  );
+  displayProduct(categorySorted);
+});
 
 // rating sorting
-const sortByRating = () => {
+ratingUp.addEventListener("click", () => {
+  const ratingSorted = products.sort((a, b) => a.rating - b.rating);
+  displayProduct(ratingSorted);
+});
+
+ratingDown.addEventListener("click", () => {
   const ratingSorted = products.sort((a, b) => b.rating - a.rating);
-  console.table("Sorted By Rating:");
-  console.table(ratingSorted);
-};
-// sortByRating();
+  displayProduct(ratingSorted);
+});
 
 // =================================================== Filter /
-// const filterCategory = "Electronics";
+filterCategory.addEventListener("input", (choice) => {
+  const filteredChoice = choice.target.value;
 
-// const filteredProduct = products.filter((product) => {
-//   return product.category === filterCategory;
-// });
-// console.log(`Filter By Category: ${filterCategory}`);
-// console.table(filteredProduct);
+  const filteredProduct =
+    filteredChoice === "All Categories"
+      ? products
+      : products.filter((product) => product.category === filteredChoice);
+
+  if (filteredProduct === "") {
+    filteredProduct = "";
+  }
+
+  console.log(filteredProduct);
+
+  displayProduct(filteredProduct);
+});
 
 // =================================================== Reduce /
 // const Total = products.reduce((totalExpens, currentPrice) => {
